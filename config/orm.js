74b71e8e955d33c1cd101a591fdd9f962a309b3d
Garrett Reichman <gsreichman@gmail.com>
2020-03-02 17:28:04 -0500

    created orms

--- config/orm.js
+++ config/orm.js
@@ -10,14 +10,14 @@ const orm = {
             cb(result);
         });
     },
-    insertOne: function (table,col,values) {
+    insertOne: function (table,col,values,cb) {
         const queryString = "INSERT INTO ??(?) VALUES ?";
         connection.query(queryString, [table,col,values], (err, result) => {
             if (err) throw err;
             cb(result);
         });
     },
-    updateOne: function (table,newCol,newVal,idCol,idVal) {
+    updateOne: function (table,newCol,newVal,idCol,idVal,cb) {
         const queryString = "UPDATE ?? SET ?=? WHERE ?=?";
         connection.query(queryString, [table,newCol,newVal,idCol,idVal], (err, result) => {
             if (err) throw err;
--- models/burger.js
+++ models/burger.js
@@ -3,7 +3,15 @@ const orm = require("../config/orm");
 
 
 const burgOrm = {
-
+    selectAllBurgers = function(cb){
+        orm.selectAll("burgers",cb);
+    },
+    insertBurger = function(name,cb){
+        orm.insertOne("burgers","burger_name",name,cb)
+    },
+    updateBurger(id,cb){
+        orm.updateOne("burgers","devoured",1,"id",id,cb);
+    }
 }
 
 
